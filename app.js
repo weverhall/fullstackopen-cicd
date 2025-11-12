7efd650e2afada33a309398fb65569f04dedeca3
w <weverhall@gmail.com>
2025-11-12 18:47:19 +0200

    11.12

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ app.listen(PORT, () => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1')
+  res.send('2')
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
